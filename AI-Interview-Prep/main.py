96e0da472a8326ab3481c999692aa1ca7a04e7d1
Aqsa Sarfraz <aqsasarfraz732@gmail.com>
2026-09-07 11:01:11 +0500

    Update main.py

--- AI-Interview-Prep/main.py
+++ AI-Interview-Prep/main.py
@@ -61,7 +61,7 @@ Use exactly this structure:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "system",
